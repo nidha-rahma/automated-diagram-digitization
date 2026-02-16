4a339bcb181360ab3d7d62e20eaabdd51cdf5f4a
nidha-rahma <nidharahma373@gmail.com>
2026-02-16 14:43:26 +0530

    arrows and rectangle shapes added to the canvas from json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from 'react';
+import ReactFlow from 'reactflow';
+import 'reactflow/dist/style.css';
+import { MarkerType } from 'reactflow';
+import flowData from './mock/sample_flow.json';
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { nodes, edges } = flowData;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <h2 style={{ textAlign: "center" }}>Flowchart Canvas</h2>
+      <ReactFlow nodes={nodes} edges={edges} fitView />
+    </div>
   );
 }
 
