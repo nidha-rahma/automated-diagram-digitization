1b4a8150570f43e68a74858f3265baef11f3f378
nidha-rahma <nidharahma373@gmail.com>
2026-02-20 02:44:23 +0530

    added custommnodes defining shapes and arrow heads

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,15 @@ import ReactFlow from 'reactflow';
 import 'reactflow/dist/style.css';
 import { MarkerType } from 'reactflow';
 import flowData from './mock/sample_flow.json';
+import { TaskNode, CircleNode, IONode, DiamondNode } from './nodes/CustomNodes';
 import "./App.css";
+
+const nodeTypes = {
+  taskNode: TaskNode,
+  circleNode: CircleNode,
+  ioNode: IONode,
+  diamondNode: DiamondNode,
+};
 
 function App() {
   const { nodes, edges } = flowData;
@@ -11,7 +19,7 @@ function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <h2 style={{ textAlign: "center" }}>Flowchart Canvas</h2>
-      <ReactFlow nodes={nodes} edges={edges} fitView />
+      <ReactFlow nodes={nodes} edges={edges} fitView nodeTypes={nodeTypes} />
     </div>
   );
 }
